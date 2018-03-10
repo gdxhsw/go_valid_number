c71ae9c486388d870dfb2f21837e80e1a6dc787f
Gdx Wu <gdx.nil@gmail.com>
2018-03-10 14:32:31 +0800

    Refactor, use different func to match different pattern

--- src/number.go
+++ src/number.go
@@ -4,19 +4,19 @@ import (
     "regexp"
 )
 
-var numeric_patterns []string
+var matchFunc []func(string) (bool, error)
 
 func IsNumber(s string) bool {
-    if numeric_patterns == nil {
-        numeric_patterns = []string{
-            "^(-|\\+|[0-9])[0-9]*$",
-            "^(((-|\\+|[0-9]+)?\\.[0-9]+)|((-|\\+|[0-9]+)+\\.[0-9]?))$",
-            "^([0-9]+e[0-9]+)$",
+    if matchFunc == nil {
+        matchFunc = []func(string) (bool, error){
+            IsInteger,
+            IsFloat,
+            IsExponentialFunction,
         }
     }
 
-    for _, pattern := range numeric_patterns {
-        is_matched, _ := regexp.MatchString(pattern, s)
+    for _, f := range matchFunc {
+        is_matched, _ := f(s)
 
         if is_matched {
             return true
@@ -24,4 +24,22 @@ func IsNumber(s string) bool {
     }
 
     return false
+}
+
+func IsInteger(s string) (bool, error) {
+    pattern := "^(-|\\+|[0-9])[0-9]*$"
+
+    return regexp.MatchString(pattern, s)
+}
+
+func IsFloat(s string) (bool, error) {
+    pattern := "^(((-|[0-9]+)?\\.[0-9]+)|((-|[0-9]+)+\\.[0-9]?))$"
+
+    return regexp.MatchString(pattern, s)
+}
+
+func IsExponentialFunction(s string) (bool, error) {
+    pattern := "^([0-9]+e[0-9]+)$"
+
+    return regexp.MatchString(pattern, s)
 }
