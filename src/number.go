eed0a306a5541da4fa8fb3721ee88cb2ae41a235
Gdx Wu <gdx.nil@gmail.com>
2018-03-10 13:45:28 +0800

    Implement IsNumber for integers

--- src/number.go
+++ src/number.go
@@ -1,5 +1,25 @@
 package number
 
+import (
+    "regexp"
+)
+
+var numeric_patterns []string
+
 func IsNumber(s string) bool {
+    if numeric_patterns == nil {
+        numeric_patterns = []string{
+            "^(-|[0-9])[0-9]*$",
+        }
+    }
+
+    for _, pattern := range numeric_patterns {
+        is_matched, _ := regexp.MatchString(pattern, s)
+
+        if is_matched {
+            return true
+        }
+    }
+
     return false
 }
